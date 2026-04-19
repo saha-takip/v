de924dd588d52d78b4126e6aabbf1a2887d88294
Ender İmen <enderimen@hotmail.com>
2026-04-20 02:19:44 +0300

    Update Footer.vue

--- src/layout/partials/Footer.vue
+++ src/layout/partials/Footer.vue
@@ -2,8 +2,8 @@
   <section class="app-footer">
     <footer class="footer">
       <div class="container-fluid clearfix">
-        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Stok Takip Programı - <a href="tel:05399135895">Ender İmen</a> Tarafından Geliştirilmiştir. ❤️️</span>
-        <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Telif hakkı © Tüm hakları saklıdır | 2025</span>
+        <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Saha Takip Programı - <a href="tel:05399135895">Ender İmen</a> Tarafından Geliştirilmiştir. ❤️️</span>
+        <span class="text-muted float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Telif hakkı © Tüm hakları saklıdır | 2025</span>
       </div>
     </footer>
   </section>
